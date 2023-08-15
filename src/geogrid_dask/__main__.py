8273022aa64d7fd4cd4f690f32ed414db3658d0f
giangblackk <giangblackk@gmail.com>
2023-08-15 16:57:30 +0700

    create hello world fastapi app

--- src/geogrid_dask/__main__.py
+++ src/geogrid_dask/__main__.py
@@ -1,11 +1,13 @@
 """Command-line interface."""
 import click
-
+import uvicorn
+from .server.main import app
 
 @click.command()
 @click.version_option()
 def main() -> None:
     """GeoGrid Dask."""
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
 
 
 if __name__ == "__main__":
