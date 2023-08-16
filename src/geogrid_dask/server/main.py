b6768309b846505217b26775b8d92e1ea5f2bb34
giangblackk <giangblackk@gmail.com>
2023-08-16 11:02:59 +0700

    use async dask client to submit job  - use dask async client to submit job inside FastAPI function and wait for result

--- src/geogrid_dask/server/main.py
+++ src/geogrid_dask/server/main.py
@@ -1,7 +1,15 @@
+from dask.distributed import Client
 from fastapi import FastAPI
 
 app = FastAPI()
 
+def inc(x: int) -> int:
+    return x + 1
+
 @app.get("/")
 async def index():
-    return {"message": "Hello, World"}
+    print("Index!!!")
+    async with Client(asynchronous=True) as client:
+        future = client.submit(inc, 10)
+        result = await future
+    return {"message": result}
