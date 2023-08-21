93526f03e917bb91096433f0aa8709812f160067
giangblackk <giangblackk@gmail.com>
2023-08-21 10:44:22 +0700

    ignore unfixable mypy warning

--- src/geogrid_dask/server/main.py
+++ src/geogrid_dask/server/main.py
@@ -8,9 +8,9 @@ def inc(x: int) -> int:
     return x + 1
 
 @app.get("/")
-async def index():
+async def index(): # type: ignore[no-untyped-def]
     print("Index!!!")
-    async with Client(asynchronous=True) as client:
+    async with Client(asynchronous=True) as client: # type: ignore[no-untyped-call]
         future = client.submit(inc, 10)
         result = await future
     return {"message": result}
